062d6492b5057a0ab06f10e976a0e31fe4690947
Christian López Espínola <penyaskito@computer.org>
2007-10-30 17:03:45 +0000

    Actualizado el ScoreBoard para que la barra se deslice al recibir daño.

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -13,15 +13,21 @@ class ScoreBoard(pygame.sprite.Sprite):
         self.pos = pos
         self.vida = 100
         pygame.draw.rect(self.screen, (0,0,255), (self.pos, 20, 200, 20))
-        pygame.draw.rect(self.screen, (0,255,255), (self.pos + 2, 22, 196, 16))
+        self.barraVida = pygame.draw.rect(self.screen, (0,255,255), \
+                (self.pos + 2, 22, 196, 16))
     
     def update(self):
         # 196 es la barra llena de vida.
         width = (196 * self.vida) / 100
+        # el elemento 2 es el width, verdad?
+        if (self.barraVida[2] != width):
+            self.barraVida[2] -= 1
         pygame.draw.rect(self.screen, (0,0,255), (self.pos, 20, 200, 20))
-        pygame.draw.rect(self.screen, (0,255,255), (self.pos + 2, 22, width, 16))
+        pygame.draw.rect(self.screen, (0,255,255), self.barraVida)
         
-    def hurt (self, score):        
+    def hurt (self, score):
+        oldVida = self.vida
         self.vida -= score
         if (self.vida < 0):
-            self.vida = 0        
+            self.vida = 0
+            
