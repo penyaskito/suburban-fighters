b2851692536f04f43835e291b99d399017785d41
Christian López Espínola <penyaskito@computer.org>
2007-11-01 21:07:45 +0000

    El timer calcula la posición donde debe mostrarse. Además, permite pausarlo y continuar, para cuando se implemente el pause.

--- Timer.py
+++ Timer.py
@@ -12,6 +12,7 @@ class Timer(pygame.sprite.Sprite):
     ESTADO_CORRIENDO = 1
     
     def __init__(self, screen):
+        self._posicion = (screen.get_width() / 2 - 20, 10)
         self._screen = screen
         self._inicial = time.time()
         self._estado = self.ESTADO_CORRIENDO
@@ -21,8 +22,7 @@ class Timer(pygame.sprite.Sprite):
     def update(self, t): 
         s = self._font.render(str("%(var)02d" % {"var":self._seconds}),True,(255,230,50))
 
-        self._screen.blit (s, (280, 10))        
-
+        self._screen.blit (s, self._posicion)        
         if self._estado == self.ESTADO_CORRIENDO:
             self._seconds = self.TIME - (time.time() - self._inicial)
         if self._seconds < 0:
@@ -31,3 +31,13 @@ class Timer(pygame.sprite.Sprite):
     
     def get_seconds(self):
         return self._seconds
+        
+    def pause(self):
+        self._estado = self.ESTADO_PARADO
+        # almacenamos el instante en el que fue parado.
+        self._stopped = time.time()
+        
+    def resume(self):
+        # calcula por donde debe seguir la cuenta.
+        self._inicial -= self._stopped - time.time()
+        self._estado = self.ESTADO_CORRIENDO
