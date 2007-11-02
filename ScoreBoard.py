f4f3b1fb82e8d90627c7412cc5193c62d947e8eb
Christian López Espínola <penyaskito@computer.org>
2007-11-02 20:37:04 +0000

    El marcador de energía muestra también el nombre del jugador (issue #5).

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -16,6 +16,8 @@ class ScoreBoard(pygame.sprite.Sprite):
         # pos es la posicion (x) donde empieza la barra
         self._pos = pos
         self._vida = 100
+        self._player_name_font = pygame.font.SysFont("Verdana",20)
+        self._player_name = "La Mari"
         pygame.draw.rect(self._screen, (0,0,255), (self._pos, 20, 200, 20))
         self._barraVida = pygame.draw.rect(self._screen, (0,255,255), \
                 (self._pos + 2, 22, 196, 16))
@@ -33,6 +35,8 @@ class ScoreBoard(pygame.sprite.Sprite):
         
         pygame.draw.rect(self._screen, (0,0,255), (self._pos, 20, 200, 20))
         pygame.draw.rect(self._screen, color, self._barraVida)
+        s = self._player_name_font.render(self.get_player_name(), True, (255,230,50))
+        self._screen.blit (s, (self._pos, 40, 200, 20))
         
     def hurt (self, score):
         oldVida = self._vida
@@ -42,3 +46,6 @@ class ScoreBoard(pygame.sprite.Sprite):
             
     def get_vida(self):
         return self._vida
+    
+    def get_player_name(self):
+        return self._player_name
