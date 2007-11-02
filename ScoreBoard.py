d7a74d98651c6166d18e2945f895d7d91450c341
Christian López Espínola <penyaskito@computer.org>
2007-11-02 00:31:54 +0000

    Modificado color de la barra de energía cuando el jugador está agonizando.

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -7,27 +7,38 @@ class ScoreBoard(pygame.sprite.Sprite):
     '''
     Clase encargada de pintar el marcador
     '''
+    
+    COLOR_FUERTE = (0,255,255)
+    COLOR_AGONICO = (255, 100, 0)
+    
     def __init__(self, screen, pos):
-        self.screen = screen
+        self._screen = screen
         # pos es la posicion (x) donde empieza la barra
-        self.pos = pos
-        self.vida = 100
-        pygame.draw.rect(self.screen, (0,0,255), (self.pos, 20, 200, 20))
-        self.barraVida = pygame.draw.rect(self.screen, (0,255,255), \
-                (self.pos + 2, 22, 196, 16))
+        self._pos = pos
+        self._vida = 100
+        pygame.draw.rect(self._screen, (0,0,255), (self._pos, 20, 200, 20))
+        self._barraVida = pygame.draw.rect(self._screen, (0,255,255), \
+                (self._pos + 2, 22, 196, 16))
     
     def update(self):
         # 196 es la barra llena de vida.
-        width = (196 * self.vida) / 100
+        width = (196 * self.get_vida()) / 100
         # el elemento 2 es el width, verdad?
-        if (self.barraVida[2] != width):
-            self.barraVida[2] -= 1
-        pygame.draw.rect(self.screen, (0,0,255), (self.pos, 20, 200, 20))
-        pygame.draw.rect(self.screen, (0,255,255), self.barraVida)
+        if (self._barraVida[2] != width):
+            self._barraVida[2] -= 1
+        if self._barraVida[2] > 30:
+            color = self.COLOR_FUERTE 
+        else:
+            color = self.COLOR_AGONICO
+        
+        pygame.draw.rect(self._screen, (0,0,255), (self._pos, 20, 200, 20))
+        pygame.draw.rect(self._screen, color, self._barraVida)
         
     def hurt (self, score):
-        oldVida = self.vida
-        self.vida -= score
-        if (self.vida < 0):
-            self.vida = 0
+        oldVida = self._vida
+        self._vida -= score
+        if (self._vida < 0):
+            self._vida = 0
             
+    def get_vida(self):
+        return self._vida
